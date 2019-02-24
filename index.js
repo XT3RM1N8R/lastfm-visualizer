685d12d40ace63c71a004d7c930fe7603bc6bd27
Darien Sokolov <darien.sokolov@yahoo.com>
2019-02-24 02:46:25 -0600

    Finally figured out basic formatted JSON to HTML in JS

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 var count = 0;
-function loadArtist() {$.ajax({
-  type : 'POST',
-  url : 'http://ws.audioscrobbler.com/2.0/',
-  data : 'method=artist.getinfo&' +
-    'artist=C418&' +
-    'api_key=b3f2f0cebcbe4c5df205da9d999fce5c&' +
-    'format=json',
-  dataType : 'jsonp',
-  success : function(data) {
-    $('#success #artistName').html(data.artist.name);
-    count = count + 1;
-    $('#success #count').html(count);
-  },
-  error : function(code, message){
-    $('#error').html('Error Code: ' + code + ', Error Message: ' + message);
-  }
-});}
+function loadArtist() {
+  jQuery.ajax({
+    type : 'GET',
+    url : 'http://ws.audioscrobbler.com/2.0/',
+    data : 'method=artist.getinfo&' +
+      'artist=C418&' +
+      'api_key=b3f2f0cebcbe4c5df205da9d999fce5c&' +
+      'format=json',
+    dataType : 'json',
+    success : function(data) {
+      $('#success #artistName').html(JSON.stringify(data, null, 2));
+      count = count + 1;
+      $('#success #count').html(count);
+    },
+    error : function(code, message){
+      $('#error').html('Error Code: ' + code + ', Error Message: ' + message);
+    }
+  });
+}
